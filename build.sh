434f45e439b852b0489a4550f1bd8336fce76571
Kelvin Tan <superkelvint@gmail.com>
2024-08-02 15:44:55 -0700

    Add text replacement scripts and deploy to static directory.

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 npm run build
 npm run build-polyfills
+
+perl -pi -e 's/Add a comment/Add a note/g' dist/recogito.min.js
+perl -pi -e 's/Add tag/Link knowledge node/g' dist/recogito.min.js
+cp dist/recogito.min.js ../ckbdesktop/static
